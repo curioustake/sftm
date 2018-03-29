20b878ec82d552175cde40c05c9f2c2a74a7ff90
Curious Take <curioustake@gmail.com>
2018-03-29 06:42:37 -0700

    Create new repo - sftm

---
+++ src/main/java/com/curioustake/sftm/App.java
@@ -0,0 +1,13 @@
+package com.curioustake.sftm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
