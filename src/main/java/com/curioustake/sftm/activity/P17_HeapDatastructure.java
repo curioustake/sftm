9f168942f8dd439f2d65da575d03b231e3bf8487
Curious Take <curioustake@gmail.com>
2018-04-24 08:04:59 -0700

    Added HeapDatastructure

---
+++ src/main/java/com/curioustake/sftm/activity/P17_HeapDatastructure.java
@@ -0,0 +1,54 @@
+package com.curioustake.sftm.activity;
+
+import com.curioustake.sftm.datastructure.Heap;
+import com.curioustake.sftm.utils.DataValidator;
+import com.curioustake.sftm.utils.RandomDataGenerator;
+
+import java.util.Arrays;
+
+
+public class P17_HeapDatastructure implements Activity {
+
+    enum SORT_ORDER { ASCENDING, DESCENDING}
+
+    public void invoke(String[] args) {
+        System.out.println( "Execute => " + Arrays.toString(args) );
+
+        final int count = Integer.parseInt(args[1]);
+        final int max = Integer.parseInt(args[2]);
+        final boolean printResults = Boolean.parseBoolean(args[3]);
+
+        Integer[] original = RandomDataGenerator.getRandomIntegerArray(count, max, printResults);
+
+        if(count <= 1) {
+            System.out.println("INPUT SIZE TO SMALL ");
+            return;
+        }
+
+        // HEAP SORT ASCENDING
+        Integer[] inputAsc = original.clone();
+        sort(inputAsc, SORT_ORDER.ASCENDING);
+        System.out.println("\n###################### HEAP SORT validateSortAscending ##############################");
+        System.out.println("\nHEAP SORT ASCENDING SUCCESSFUL? [" + DataValidator.validateSortAscending(original, inputAsc, printResults) + "]\n");
+        System.out.println("############################################################################################\n");
+
+        // HEAP SORT DESCENDING
+        Integer[] inputDesc = original.clone();
+        sort(inputDesc, SORT_ORDER.DESCENDING);
+        System.out.println("\n###################### HEAP SORT validateSortDescending #############################");
+        System.out.println("\nHEAP SORT DESCENDING SUCCESSFUL? [" + DataValidator.validateSortDescending(original, inputDesc, printResults) + "]\n");
+        System.out.println("############################################################################################\n");
+
+    }
+
+    private void sort(Integer[] input, SORT_ORDER sortOrder) {
+
+        Heap heap = new Heap(sortOrder.equals(SORT_ORDER.ASCENDING) ? Heap.HEAP_TYPE.MIN : Heap.HEAP_TYPE.MAX);
+
+        Arrays.stream(input).forEach(i->{heap.put(i);});
+
+        for(int i=0; i<input.length; i++) {
+            input[i] = heap.remove();
+        }
+    }
+}
---
+++ src/main/java/com/curioustake/sftm/datastructure/Heap.java
@@ -0,0 +1,106 @@
+package com.curioustake.sftm.datastructure;
+
+import java.util.Arrays;
+
+public class Heap {
+
+    public enum HEAP_TYPE{ MIN, MAX}
+
+    private static final int DEFAULT_SIZE = 10;
+
+    Integer[] data = new Integer[DEFAULT_SIZE];
+    int size = -1;
+    HEAP_TYPE type;
+
+    public Heap(final HEAP_TYPE type) {
+        this.type = type;
+    }
+
+    public Integer get() {
+        if(size<0)
+            return -1;
+
+        return data[0];
+    }
+
+    public Integer remove() {
+        if(size<0)
+            return -1;
+
+        int top = data[0];
+
+        data[0] = data[size];
+        size--;
+
+        heapifyChild(0);
+
+        return top;
+    }
+
+    public void put(final Integer value) {
+        size++;
+
+        if(size > (data.length-1)) {
+            data = Arrays.copyOf(data, data.length*2);
+        }
+
+        data[size] = value;
+
+        heapifyParent(size);
+    }
+
+    public String toString() {
+        return Arrays.toString(Arrays.copyOfRange(data,0, size+1));
+    }
+
+    private void heapifyParent(final int node) {
+
+        if(node <= 0)
+            return;
+
+        Double parentIndex = Math.floor((node - 1)/2);
+
+        if(isValidHeap(data[node], data[parentIndex.intValue()])) {
+            swap(parentIndex.intValue(), node);
+            heapifyParent(parentIndex.intValue());
+        }
+    }
+
+    private void heapifyChild(final int node) {
+        if(size <= 0)
+            return;
+
+        Double lastParentNode = Math.floor((size-1)/2);
+
+        if(node>lastParentNode)
+            return;
+
+        int leftChild = ((node*2) + 1) > size ? -1 : (node*2) + 1 ;
+        int rigthChild = leftChild + 1 > size ? -1 : leftChild + 1 ;
+
+        if((rigthChild > -1) && isValidHeap(data[rigthChild], data[leftChild]) && isValidHeap(data[rigthChild], data[node])) {
+            swap(node, rigthChild);
+            heapifyChild(rigthChild);
+        } else if(isValidHeap(data[leftChild], data[node])) {
+            swap(node, leftChild);
+            heapifyChild(leftChild);
+        }
+    }
+
+    private boolean isValidHeap(final int first, final int second) {
+        switch (type) {
+            case MAX:
+                return first > second;
+            case MIN:
+                return first < second;
+            default:
+                throw new RuntimeException("Invalid Heap Type");
+        }
+    }
+
+    private void swap(final int first, final int second) {
+        Integer temp = data[second];
+        data[second] = data[first];
+        data[first] = temp;
+    }
+}
