b0f5a2aa67ef5de418461b7f235607a780fe0c1a
Curious Take <curioustake@gmail.com>
2018-05-05 13:20:20 -0700

    Added QuickSelectOrderStatistic

---
+++ src/main/java/com/curioustake/sftm/activity/P24_QuickSelectOrderStatistic.java
@@ -0,0 +1,74 @@
+package com.curioustake.sftm.activity;
+
+import com.curioustake.sftm.utils.RandomDataGenerator;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Purpose : Select the n order static in a given input
+ *
+ * Complexity (Time): O(n log(n))
+ * */
+
+public class P24_QuickSelectOrderStatistic implements Activity {
+
+    public void invoke(String[] args) {
+        System.out.println( "Execute => " + Arrays.toString(args) );
+
+        final int count = Integer.parseInt(args[1]);
+        final int max = Integer.parseInt(args[2]);
+        final boolean printResults = Boolean.parseBoolean(args[3]);
+
+        Random random = new Random();
+        final int orderStatisticIndex = random.nextInt(count);
+
+        Integer[] original = RandomDataGenerator.getRandomIntegerArray(count, max, printResults);
+
+        if(count <= 1) {
+            System.out.println("INPUT SIZE TO SMALL ");
+            return;
+        }
+
+        Integer[] treatment = original.clone();
+        int orderStatistic = getOrderStatistic(treatment, 0, treatment.length-1, orderStatisticIndex);
+        System.out.println("\n###################### Quick Select Order Statistic validate ##############################");
+        Integer[] control = original.clone();
+        Arrays.sort(control);
+        System.out.println("\nQuick Select Order Statistic Control [" + orderStatisticIndex + "] = [" + control[orderStatisticIndex] + "]");
+        System.out.println("\nQuick Select Order Statistic Treatment [" + orderStatisticIndex + "] = [" + orderStatistic + "]");
+        System.out.println("############################################################################################\n");
+    }
+
+    private int getOrderStatistic(Integer[] input, int start, int end, int orderStatisticIndex) {
+        if((end - start) == 0)
+            return input[start];
+
+        Random random = new Random();
+        int randomPivotIndex = start + random.nextInt(end - start);
+        swap(input, randomPivotIndex, end);
+
+        int j=start;
+        for(int i=start; i< end; i++) {
+            if(input[i] < input[end]) {
+                swap(input, j, i);
+                j++;
+            }
+        }
+        swap(input, j, end);
+
+        if(j == orderStatisticIndex)
+            return input[j];
+        else if(j> orderStatisticIndex)
+            return getOrderStatistic(input, start, j-1, orderStatisticIndex);
+        else
+            return getOrderStatistic(input, j+1, end, orderStatisticIndex);
+    }
+
+    private void swap(Integer[] input, int first, int second) {
+        int temp = input[first];
+        input[first] = input[second];
+        input[second] = temp;
+    }
+
+}
--- src/main/java/com/curioustake/sftm/utils/RandomDataGenerator.java
+++ src/main/java/com/curioustake/sftm/utils/RandomDataGenerator.java
@@ -6,18 +6,6 @@ import java.util.Random;
 public class RandomDataGenerator {
 
     public static Integer[] getRandomIntegerArray(final int count, final int max, final boolean printResults) {
-//        Integer[] randomIntegers = new Integer[count];
-//
-//        final Random random = new Random();
-//
-//        for(int i=0; i<count; i++)
-//            randomIntegers[i] = random.nextInt(max);
-//
-//        if(printResults)
-//            System.out.println("RANDOM INTEGERS : " + Arrays.toString(randomIntegers));
-//
-//        return randomIntegers;
-//
         return getRandomIntegerArray(count, 0, max, printResults);
     }
 
