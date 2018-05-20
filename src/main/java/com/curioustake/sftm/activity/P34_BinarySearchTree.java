c16b3b3e12a487f7bc2c0c186d5d830cac5c26e4
Curious Take <curioustake@gmail.com>
2018-05-20 00:46:16 -0700

    Added RBinarySearchTree

---
+++ src/main/java/com/curioustake/sftm/activity/P34_BinarySearchTree.java
@@ -0,0 +1,113 @@
+package com.curioustake.sftm.activity;
+
+import com.curioustake.sftm.datastructure.RBinarySearchTree;
+import com.curioustake.sftm.utils.RandomDataGenerator;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class P34_BinarySearchTree implements Activity {
+    enum OPERATIONS {PUT, GET, REMOVE, SUCCESSOR, PREDECESSOR, CONTAINS_KEY, KEY_SET, IS_EMPTY}
+
+    Map<OPERATIONS, Integer> operationCount = new HashMap();
+
+    public void invoke(String[] args) {
+        System.out.println( "Execute => " + Arrays.toString(args) );
+
+        final int count = Integer.parseInt(args[1]);
+        final int max = Integer.parseInt(args[2]);
+        final boolean printResults = Boolean.parseBoolean(args[3]);
+
+        Integer[] original = RandomDataGenerator.getRandomIntegerArray(count, max, printResults);
+
+        if(count <= 1) {
+            System.out.println("INPUT SIZE TO SMALL ");
+            return;
+        }
+
+        TreeMap<Integer, Integer> control = new TreeMap<>();
+
+        RBinarySearchTree<Integer, Integer> treatment = new RBinarySearchTree<>();
+
+        Random random = new Random();
+
+        for(int i=0; i<original.length;) {
+            OPERATIONS operation = control.isEmpty() ? OPERATIONS.PUT : OPERATIONS.values()[random.nextInt(OPERATIONS.IS_EMPTY.ordinal()  + 1)];
+            int inputValue = original[i];
+
+//            System.out.println(operation + "|" + inputValue);
+
+            switch (operation) {
+                case PUT:
+                    Integer putControl = control.put(inputValue, inputValue);
+                    Integer putTreatment = treatment.put(inputValue, inputValue);
+
+                    if((putControl != null && putTreatment != null) && !putControl.equals(putTreatment))
+                        throwException(operation.name(), control, treatment);
+                    i++;
+                    break;
+                case GET:
+                    if((control.get(inputValue) != null && treatment.get(inputValue) != null) &&
+                            (!control.get(inputValue).equals(treatment.get(inputValue))))
+                        throwException(operation.name(), control, treatment);
+                    break;
+                case REMOVE:
+                    Integer removeControl = control.remove(inputValue);
+                    Integer removeTreatment = treatment.remove(inputValue);
+
+                    if((removeControl != null && removeTreatment != null) && !removeControl.equals(removeTreatment))
+                        throwException(operation.name(), control, treatment);
+                    break;
+                case SUCCESSOR:
+                    if((control.higherKey(inputValue) != null && treatment.higherKey(inputValue) != null) &&
+                            (!control.higherKey(inputValue).equals(treatment.higherKey(inputValue))))
+                        throwException(operation.name(), control, treatment);
+                    break;
+                case PREDECESSOR:
+                    if((control.lowerKey(inputValue) != null && treatment.lowerKey(inputValue) != null) &&
+                            (!control.lowerKey(inputValue).equals(treatment.lowerKey(inputValue))))
+                        throwException(operation.name(), control, treatment);
+                    break;
+                case CONTAINS_KEY:
+                    if(control.containsKey(inputValue) != treatment.containsKey(inputValue))
+                        throwException(operation.name(), control, treatment);
+                    break;
+                case KEY_SET:
+                    if(!Arrays.equals(control.keySet().toArray(), treatment.keySet().toArray()))
+                        throwException(operation.name(), control, treatment);
+                    break;
+                case IS_EMPTY:
+                    if(control.isEmpty() != treatment.isEmpty())
+                        throwException(operation.name(), control, treatment);
+                    break;
+                default:
+                    throw new RuntimeException("Unknown operation for BinarySearchTrees" + operation);
+            }
+
+            if(!Arrays.equals(control.keySet().toArray(), treatment.keySet().toArray()))
+                throwException(operation.name(), control, treatment);
+
+            int opCount = 1;
+
+            if(operationCount.containsKey(operation))
+                opCount = operationCount.get(operation) + 1;
+
+            operationCount.put(operation, opCount);
+        }
+
+        System.out.println("\nValidated BinarySearchTree implementation. Operations run....");
+
+        operationCount.entrySet().stream().forEach(k -> System.out.println( k.getKey() + " | " + k.getValue()));
+    }
+
+    private void throwException(final String operation, Map<Integer, Integer> control, RBinarySearchTree<Integer, Integer> treatment) {
+        System.out.println("\nFAILED OPERATION [" + operation + "]");
+        System.out.println("CONTROL" + Arrays.toString(control.keySet().toArray()));
+        System.out.println("TREATMENT " + Arrays.toString(treatment.keySet().toArray()) );
+
+        throw new RuntimeException("BinarySearchTree operation " + operation + " not working ");
+    }
+}
---
+++ src/main/java/com/curioustake/sftm/datastructure/RBinarySearchTree.java
@@ -0,0 +1,223 @@
+package com.curioustake.sftm.datastructure;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class RBinarySearchTree <K extends Comparable<K>, V> {
+
+    Node root;
+    int size;
+    Node sentinel;
+
+    public <K, V> RBinarySearchTree() {
+        root = null;
+        size = 0;
+        sentinel = new Node();
+    }
+
+    public V put(K key, V value) {
+        Node newNode = new Node(key, value);
+
+        V previous = null;
+
+        if(root == null) {
+            root = newNode;
+        } else {
+            previous = put(root, newNode);
+        }
+
+        size++;
+        return previous;
+    }
+
+    private V put(Node subTreeRoot, Node newNode) {
+        V previous = null;
+
+        if(subTreeRoot.key_.compareTo(newNode.key_) == 0) {
+            previous = subTreeRoot.value_;
+            subTreeRoot.value_ = newNode.value_;
+        } else if (subTreeRoot.key_.compareTo(newNode.key_) > 0) {
+            if(subTreeRoot.left_ == sentinel) {
+                newNode.parent_ = subTreeRoot;
+                subTreeRoot.left_ = newNode;
+            }
+            else
+                return put(subTreeRoot.left_, newNode);
+        } else {
+            if(subTreeRoot.right_ == sentinel) {
+                newNode.parent_ = subTreeRoot;
+                subTreeRoot.right_ = newNode;
+            }
+            else
+                return put(subTreeRoot.right_, newNode);
+        }
+
+        return previous;
+    }
+
+    public V get(K key) {
+        if(isEmpty())
+            return null;
+
+        Node node = search(root, key);
+        return node == null ? null : node.value_;
+    }
+
+    public V remove(K key) {
+        if(isEmpty())
+            return null;
+
+        Node node = search(root, key);
+
+        if(node == null)
+            return null;
+
+        if(node.right_ == sentinel || node.left_ == sentinel) {
+            return removeLeafOrSingleChildNode(node);
+        }
+
+        Node successor = higherNode(node);
+
+        if(successor == null) {
+            throw new RuntimeException("Code cannot be reached");
+        }
+
+        V temp = node.value_;
+
+        node.key_ = successor.key_;
+        node.value_ = successor.value_;
+
+        removeLeafOrSingleChildNode(successor);
+
+        return temp;
+    }
+
+    private V removeLeafOrSingleChildNode(Node node) {
+        Node child = sentinel;
+
+        if(node.right_ != sentinel)
+            child = node.right_;
+        else if(node.left_ != sentinel)
+            child = node.left_;
+
+        if(node != root) {
+            if(node.parent_.right_ == node)
+                node.parent_.right_ = child;
+            else
+                node.parent_.left_ = child;
+
+            if(child != sentinel)
+                child.parent_ = node.parent_;
+        } else {
+            root = child == sentinel ? null : child;
+        }
+
+        size--;
+        return node.value_;
+    }
+
+    private Node search(Node subTreeRoot, K key) {
+        if(subTreeRoot == sentinel)
+            return null;
+
+        if(subTreeRoot.key_.compareTo(key) == 0) {
+            return subTreeRoot;
+        } else if(subTreeRoot.key_.compareTo(key) > 0) {
+            return search(subTreeRoot.left_, key);
+        } else {
+            return search(subTreeRoot.right_, key);
+        }
+    }
+
+    public K higherKey(K key) {
+        Node node = search(root, key);
+        return higherNode(node) == null ? null : higherNode(node).key_;
+    }
+
+    private Node higherNode(Node node) {
+        if(node == null || node.right_ == sentinel)
+            return null;
+
+        Node successor = node.right_;
+
+        while(successor.left_ != sentinel) {
+            successor = successor.left_;
+        }
+
+        return successor;
+    }
+
+    public K lowerKey(K key) {
+        Node node = search(root, key);
+        return lowerNode(node) == null ? null : lowerNode(node).key_;
+    }
+
+    private Node lowerNode(Node node) {
+        if(node == null || node.left_ == sentinel)
+            return null;
+
+        Node predecessor = node.left_;
+
+        while(predecessor.right_ != sentinel) {
+            predecessor = predecessor.right_;
+        }
+
+        return predecessor;
+    }
+
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    public Set<K> keySet() {
+        return new LinkedHashSet<>(keySet(root));
+    }
+
+    private List<K> keySet(Node node) {
+        List<K> keys = new LinkedList<>();
+
+        if(node == null && node != sentinel)
+            return keys;
+
+        if(node.left_ != sentinel)
+            keys.addAll(keySet(node.left_));
+
+        keys.add(node.key_);
+
+        if(node.right_ != sentinel)
+            keys.addAll(keySet(node.right_));
+
+        return keys;
+    }
+
+    public boolean isEmpty() {
+        return size <= 0;
+    }
+
+    class Node {
+        Node parent_;
+        Node left_;
+        Node right_;
+
+        K key_;
+        V value_;
+
+        Node(){
+            parent_ = null;
+            left_ = null;
+            right_ = null;
+            key_ = null;
+            value_ = null;
+        }
+
+        Node(K key, V value) {
+            parent_ = null;
+            left_ = sentinel;
+            right_ = sentinel;
+            key_ = key;
+            value_ = value;
+        }
+    }
+}
